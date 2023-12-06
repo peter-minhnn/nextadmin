d1547b1b345bc1200d7ca241b793aa4ec16d752c
MinhNN <nnminh742@gmail.com>
2023-12-06 20:57:25 +0700

    refactor nextjs 14 project

--- api/auth.ts
+++
@@ -1,5 +0,0 @@
-import { axiosWrapper } from "@/api/axios-wrapper";
-import { requestHandler } from "@/api/request-handler";
-import { LoginRequest, User } from "@/types/login-type";
-
-export const authenticate = requestHandler<LoginRequest, User>((params?: any) => axiosWrapper.post('/auth/staff', params));
--- lib/actions/user-action.ts
+++
@@ -1,9 +0,0 @@
-'use server'
-import { axiosWrapper } from "@/api/axios-wrapper";
-import { requestHandler } from "@/api/request-handler";
-
-const getUsers = requestHandler<any, any>(() => axiosWrapper.get('/user'));
-
-export {
-    getUsers
-}
