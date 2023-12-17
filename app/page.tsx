b8bb7bae20d53d5c4bd4155a18f82e0c28c18ab6
MinhNN <nnminh742@gmail.com>
2023-12-17 22:25:40 +0700

    update layout

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image'
-
-export default async function Home() {
-  return (
-    <>HOME</>
-  )
-}
--- lib/actions/auth-action.ts
+++
@@ -1,7 +0,0 @@
-import { axiosWrapper } from "@/api/axios-wrapper";
-import { requestHandler } from "@/api/request-handler";
-import { BaseReponse } from "@/types/base-type";
-import { LoginRequest } from "@/types/login-type";
-
-export const authenticate = requestHandler<LoginRequest, BaseReponse>((params?: any) => axiosWrapper.post('/auth/staff', params));
-export const isValidToken = requestHandler<{ token: string }, BaseReponse>((params?: any) => axiosWrapper.post('/auth/valid-token', params));
